31ea6e63680c93b5b329fb2c732f53a1832640ae
Prashant Balotra <Prashant.Balotra@bcstechnology.com.au>
2024-05-10 08:34:02 +0530

    Easy 1

---
+++ src/arrays/easy/LargestElementInArray.java
@@ -0,0 +1,18 @@
+package arrays.easy;
+
+public class LargestElementInArray {
+    static int largestElement(int[] arr, int n) {
+        // Write your code here.
+        int largest = Integer.MIN_VALUE;
+
+        for(int i=0; i<n; i++) {
+            largest = Math.max(largest, arr[i]);
+        }
+        return largest;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 2, 5, 1, 7};
+        System.out.println(largestElement(arr, 5));
+    }
+}
