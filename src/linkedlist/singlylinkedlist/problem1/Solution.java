076e61fdff29f2af6367739497bebc3c076d38bf
Prashant Balotra <69639884+balotraprashant@users.noreply.github.com>
2024-05-16 00:47:45 +0530

    Update Solution.java

--- src/linkedlist/singlylinkedlist/problem1/Solution.java
+++ src/linkedlist/singlylinkedlist/problem1/Solution.java
@@ -1,4 +1,60 @@
 package linkedlist.singlylinkedlist.problem1;
 
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node() { data = 0; }
+    Node(int d) { data = d; }  //constructor to create a new node
+}
 public class Solution {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+            Node ans = ConstructUtil.constructLL(a);
+            while (ans != null) {
+                System.out.print(ans.data + " ");
+                ans = ans.next;
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node() { data = 0; }
+    Node(int d) { data = d; }  //constructor to create a new node
+}
+*/
+class ConstructUtil {
+    static Node constructLL(int arr[]) {
+        Node head = new Node(arr[0]);
+        Node curr = head;
+
+        for(int i=1; i<arr.length; i++) {
+            curr.next = new Node(arr[i]);
+            curr = curr.next;
+        }
+
+        return head;
+    }
 }
