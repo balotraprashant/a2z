c8029f3b665fc8bfce9b9693fbfa45abf0c38a9b
Prashant Balotra <69639884+balotraprashant@users.noreply.github.com>
2024-05-17 21:50:25 +0530

    Update Solution.java

--- src/linkedlist/singlylinkedlist/problem3/Solution.java
+++ src/linkedlist/singlylinkedlist/problem3/Solution.java
@@ -1,4 +1,16 @@
 package linkedlist.singlylinkedlist.problem3;
 
-public class Solution {
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
 }
