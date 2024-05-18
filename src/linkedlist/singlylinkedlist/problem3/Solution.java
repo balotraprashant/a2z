ee58613220b306299d163438c90c04fe0503872e
Prashant Balotra <69639884+balotraprashant@users.noreply.github.com>
2024-05-18 17:57:48 +0530

    Update Solution.java

--- src/linkedlist/singlylinkedlist/problem3/Solution.java
+++ src/linkedlist/singlylinkedlist/problem3/Solution.java
@@ -13,4 +13,21 @@ class Solution {
         node.val = node.next.val;
         node.next = node.next.next;
     }
+
+    // Function to delete the tail of the linked list
+    public static Node deleteTail(ListNode head) {
+        // Check if the linked list is empty or has only one node
+        if (head == null || head.next == null)
+            return null;
+        // Create a temporary pointer for traversal
+        ListNode temp = head;
+        // Traverse the list until the second-to-last node
+        while (temp.next.next != null) {
+            temp = temp.next;
+        }
+        // Nullify the connection from the second-to-last node to delete the last node
+        temp.next = null;
+        // Return the updated head of the linked list
+        return head;
+    }
 }
