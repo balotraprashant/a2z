fddc604739bf5be5b40c8bc236dd9dfc2acd6752
Prashant Balotra <69639884+balotraprashant@users.noreply.github.com>
2024-05-18 23:44:03 +0530

    Update Solution.java

--- src/linkedlist/singlylinkedlist/problem5/Solution.java
+++ src/linkedlist/singlylinkedlist/problem5/Solution.java
@@ -1,4 +1,51 @@
 package linkedlist.singlylinkedlist.problem5;
 
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+
+} 
+
 public class Solution {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0) {
+            int n = sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            for (int i = 0; i < n - 1; i++) {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            int key = sc.nextInt();
+            LinkedList g = new LinkedList();
+            boolean ans = g.searchKey(n, head, key);
+            System.out.println(ans ? "1" : "0");
+            // printList(head);
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+class LinkedList {
+    static boolean searchKey(int n, Node head, int key) {
+        // Code here
+        while(head!=null){
+            if(head.data==key) return true;
+            
+            head=head.next;
+        }
+        
+        return false;
+    }
 }
