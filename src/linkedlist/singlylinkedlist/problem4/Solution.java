6261c1363346b26b92d223e1269d41d7fd2813ce
Prashant Balotra <69639884+balotraprashant@users.noreply.github.com>
2024-05-18 18:42:09 +0530

    Update Solution.java

--- src/linkedlist/singlylinkedlist/problem4/Solution.java
+++ src/linkedlist/singlylinkedlist/problem4/Solution.java
@@ -1,4 +1,53 @@
 package linkedlist.singlylinkedlist.problem4;
 
-public class Solution {
+import java.util.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int a){
+        data = a;
+        next = null;
+    }
+    
+}
+
+class LinkedList
+{
+    //Function to count nodes of a linked list.
+    public static int getCount(Node head)
+    {
+        int count = 0;
+        while(head!=null) {
+            head = head.next;
+            count++;
+        }
+        return count;
+        //Code here
+    }
+}
+    
+
+//{ Driver Code Starts.
+public class Solution{
+    
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0)
+        {
+            int n=sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            for(int i=0; i<n-1; i++)
+            {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            
+            LinkedList ob = new LinkedList();
+            System.out.println(ob.getCount(head));
+        }
+    }   
 }
