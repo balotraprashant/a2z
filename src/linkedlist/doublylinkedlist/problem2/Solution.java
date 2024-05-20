66656104cbd1910e5739aeb47bf0faf77352a97a
Prashant Balotra <69639884+balotraprashant@users.noreply.github.com>
2024-05-20 23:09:47 +0530

    Update Solution.java

--- src/linkedlist/doublylinkedlist/problem2/Solution.java
+++ src/linkedlist/doublylinkedlist/problem2/Solution.java
@@ -1,5 +1,142 @@
 package linkedlist.doublylinkedlist.problem2;
 
-public class Solution {
+//{ Driver Code Starts
+import java.util.*;
 
+class Node
+{
+	int data;
+	Node next;
+	Node prev;
+	Node(int data)
+	{
+	    this.data = data;
+	    next = prev = null;
+	}
+}
+
+public class Solution
+{
+	
+	Node newNode(Node head, int data)
+	{
+		Node n = new Node(data);
+		if(head == null)
+		{
+		    head = n;
+		    return head;
+		}
+		head.next = n;
+		n.prev = head;
+		head = n;
+		return head;
+	}
+	
+	
+	void printList(Node node)
+	{
+		Node temp = node;
+		while(temp.next != null)
+		{
+			temp = temp.next;
+		}
+		
+		while(temp.prev != null)
+		{
+			temp = temp.prev;
+		}
+		
+		while(temp != null)
+		{
+			System.out.print(temp.data+" ");
+			temp = temp.next;
+		}
+		System.out.println();
+	}
+	
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		DLinkedList DLL = new DLinkedList();
+		int t = sc.nextInt();
+		
+		while(t>0)
+		{
+			Node temp;
+			Node head = null;
+			Node root = null;
+			int n = sc.nextInt();
+			for(int i=0;i<n;i++)
+			{
+				int x = sc.nextInt();
+				head = DLL.newNode(head,x);
+			    if(root == null)    root = head;
+			}
+			head = root;
+			int pos = sc.nextInt();
+			int data = sc.nextInt();
+			
+		    DLinkedList g = new DLinkedList();
+		    g.addNode(head,pos,data);
+			
+			DLL.printList(head);
+			while(head.next != null)
+			{
+				temp = head;
+				head = head.next;
+			}
+		    t--;
+		}
+	}
+}
+// } Driver Code Ends
+
+
+/* Structure of Doubly Linked List
+class Node
+{
+	int data;
+	Node next;
+	Node prev;
+	Node(int data)
+	{
+	    this.data = data;
+	    next = prev = null;
+	}
+}*/
+
+class DLinkedList
+{
+    //Function to insert a new node at given position in doubly linked list.
+    void addNode(Node head, int pos, int data)
+	{
+		// Your code here
+		Node newNode = new Node(data);
+		
+		if(head==null) {
+		    head = newNode;    
+		}
+		
+		Node curr = head;
+		
+		int i=1;
+		
+		while(curr!=null && i<=pos) {
+		    i++;
+		    curr = curr.next;
+		}
+		
+		if(curr==null) return;
+		
+		if(curr.next==null) {
+		    curr.next = newNode;
+		    newNode.prev = curr;
+		} else {
+		    Node ptr = curr.next;
+		    curr.next = newNode;
+		    ptr.prev = newNode;
+		    newNode.next = ptr;
+		    newNode.prev = curr;
+		}
+	}
 }
