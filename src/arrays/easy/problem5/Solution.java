ffea0a6c68bc057cc7bdb92f0bb409c1b7f43250
Prashant Balotra <69639884+balotraprashant@users.noreply.github.com>
2024-05-25 02:17:04 +0530

    Update Solution.java

--- src/arrays/easy/problem5/Solution.java
+++ src/arrays/easy/problem5/Solution.java
@@ -3,7 +3,7 @@ package arrays.easy.problem5;
 class Solution {
 
     // Brute Force Approach
-    public void rotateRight(int[] nums, int k) {
+    public void rotate(int[] nums, int k) {
         int n = nums.length;
         // int[] res = new int[n];
 
@@ -30,45 +30,61 @@ class Solution {
 
         /* Next Approach */
 
-        if (n == 0) {
-            return;
-        }
+        // if (n == 0) {
+        //     return;
+        // }
 
-        // Take modulus for checking if k is divisible by n, else value of k will be same
-        k = k % n;
+        // // Take modulus for checking if k is divisible by n, else value of k will be same
+        // k = k % n;
         
-        // if k will be greater than n when k/n > n, it means numsay will be rotated by n elements more than n times 
-        if (k > n) {
-            return;
-        }
+        // // if k will be greater than n when k/n > n, it means numsay will be rotated by n elements more than n times 
+        // if (k > n) {
+        //     return;
+        // }
 
-        int[] temp = new int[k];
-        //                                  4      -4         4
-        // iteration 1: n = 7, k = 3, temp[(n-k) - n+k] = nums[n-k] = temp[0] = nums[4];
+        // int[] temp = new int[k];
+        // //                                  4      -4         4
+        // // iteration 1: n = 7, k = 3, temp[(n-k) - n+k] = nums[n-k] = temp[0] = nums[4];
         
-        //                                  5      -4         5
-        // iteration 2: n = 7, k = 3, temp[(n-k) - n+k] = nums[n-k] = temp[1] = nums[5];
+        // //                                  5      -4         5
+        // // iteration 2: n = 7, k = 3, temp[(n-k) - n+k] = nums[n-k] = temp[1] = nums[5];
 
-        //                                  6      -4         6
-        // iteration 3: n = 7, k = 3, temp[(n-k) - n+k] = nums[n-k] = temp[2] = nums[6];
-        for (int i = n - k; i < n; i++) {
-            temp[i - n + k] = nums[i];
-        }
+        // //                                  6      -4         6
+        // // iteration 3: n = 7, k = 3, temp[(n-k) - n+k] = nums[n-k] = temp[2] = nums[6];
+        // for (int i = n - k; i < n; i++) {
+        //     temp[i - n + k] = nums[i];
+        // }
 
-        //                                  4      -4         4
-        // iteration 1: n = 7, k = 3, temp[(n-k) - n+k] = nums[n-k] = temp[0] = nums[4];
+        // //                                  4      -4         4
+        // // iteration 1: n = 7, k = 3, temp[(n-k) - n+k] = nums[n-k] = temp[0] = nums[4];
         
-        //                                  5      -4         5
-        // iteration 2: n = 7, k = 3, temp[(n-k) - n+k] = nums[n-k] = temp[1] = nums[5];
+        // //                                  5      -4         5
+        // // iteration 2: n = 7, k = 3, temp[(n-k) - n+k] = nums[n-k] = temp[1] = nums[5];
 
-        //                                  6      -4         6
-        // iteration 3: n = 7, k = 3, temp[(n-k) - n+k] = nums[n-k] = temp[2] = nums[6];
-        for (int i = n - k - 1; i >= 0; i--) {
-            nums[i + k] = nums[i];
-        }
+        // //                                  6      -4         6
+        // // iteration 3: n = 7, k = 3, temp[(n-k) - n+k] = nums[n-k] = temp[2] = nums[6];
+        // for (int i = n - k - 1; i >= 0; i--) {
+        //     nums[i + k] = nums[i];
+        // }
 
-        for (int i = 0; i < k; i++) {
-            nums[i] = temp[i];
+        // for (int i = 0; i < k; i++) {
+        //     nums[i] = temp[i];
+        // }
+
+        // Best Approach
+        k %= n;
+        reverse(nums, n-k, n-1);
+        reverse(nums, 0, n-k-1);
+        reverse(nums, 0, n-1);
+    }
+
+    public void reverse(int[] arr, int start, int end) {
+        while(start<=end && start>=0 && end<arr.length) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
         }
     }
 }
