ca4b5b30c4b4fa40bc61922a6d25f15d366ab81b
Prashant Balotra <69639884+balotraprashant@users.noreply.github.com>
2024-05-25 16:15:39 +0530

    Update Solution.java

--- src/arrays/easy/problem6/Solution.java
+++ src/arrays/easy/problem6/Solution.java
@@ -1,4 +1,40 @@
 package arrays.easy.problem6;
 
-public class Solution {
+class Solution {
+    public void moveZeroes(int[] nums) {
+        // int n=nums.length;
+        // int left = 0;
+        // for(int i=0; i<n; i++) {
+        //     if(nums[i]==0) {
+        //         left = i;
+        //         break;
+        //     } else if(i==n-1) {
+        //         return;
+        //     }
+        // }
+
+        // int right = left+1;
+
+        // while(right<n) {
+        //     if(nums[right]!=0) {
+        //         nums[left] = nums[right];
+        //         nums[right] = 0;
+        //         left++;
+        //         right++;
+        //     } else {
+        //         right++;
+        //     }
+        // }
+
+        int left = 0;
+        for (int right = 0 ; right < nums.length ; right++) {
+            if (nums[right] != 0) {
+                nums[left++] = nums[right];
+            }
+        }
+
+        for (; left < nums.length ; left++) {
+            nums[left] = 0;
+        }
+    }
 }
