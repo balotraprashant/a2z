3df806401be6a5bc09b3439c909cccce3c0f8757
Prashant Balotra <69639884+balotraprashant@users.noreply.github.com>
2024-05-30 23:09:54 +0530

    Update Solution.java

--- src/arrays/easy/problem10/Solution.java
+++ src/arrays/easy/problem10/Solution.java
@@ -1,4 +1,18 @@
 package arrays.easy.problem10;
 
 public class Solution {
+  public int findMaxConsecutiveOnes(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int count = 0;
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i]==1) {
+                count++;
+            } else {
+                max = Math.max(max, count);
+                count = 0;
+            }
+        }
+
+        return Math.max(max, count);
+    }
 }
