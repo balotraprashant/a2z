7230ea5090ef94ad12130e6db92b824820a823db
Prashant Balotra <69639884+balotraprashant@users.noreply.github.com>
2024-06-02 23:11:44 +0530

    Update Solution.java

--- src/arrays/easy/problem12/Solution.java
+++ src/arrays/easy/problem12/Solution.java
@@ -1,4 +1,25 @@
 package arrays.easy.problem12;
 
 public class Solution {
+    // Function for finding maximum and value pair
+    // Brute Force Approach
+    public static int lenOfLongSubarr (int A[], int N, int K) {
+        //Complete the function
+        int maxLength = Integer.MIN_VALUE;
+        int currLength = 0;
+        
+        for(int i=0; i<N; i++) {
+            int sum = 0;
+            for(int j=i; j<N; j++) {
+                sum += A[j];
+                
+                currLength = j-i+1;
+                if(sum==K) {
+                    maxLength = Math.max(maxLength, currLength);
+                }
+            }
+        }
+        
+        return maxLength;
+    }
 }
