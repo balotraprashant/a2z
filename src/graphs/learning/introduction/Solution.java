15597e448464b9a4d9453ab9db3cc3243b6c1596
Prashant Balotra <69639884+balotraprashant@users.noreply.github.com>
2024-06-05 23:58:27 +0530

    Update Solution.java

--- src/graphs/learning/introduction/Solution.java
+++ src/graphs/learning/introduction/Solution.java
@@ -19,6 +19,9 @@ class Driver {
   }
 }
 
+// There are total n*(n-1)/2 possible edges. For every edge, there are to possible options, either we pick it or don\'t pick.
+// So total number of possible graphs is 2n(n-1)/2.
+
 public class Solution {
   static long count(int n) {
       return (long) Math.pow(2, (n*(n-1))/2);
