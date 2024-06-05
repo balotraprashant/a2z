c94ac87d0124e2674fcee4845a7a595aba938c69
Prashant Balotra <69639884+balotraprashant@users.noreply.github.com>
2024-06-05 23:32:16 +0530

    Update Solution.java

--- src/graphs/learning/introduction/Solution.java
+++ src/graphs/learning/introduction/Solution.java
@@ -1,4 +1,27 @@
 package graphs.learning.introduction;
 
-public class Solution {
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class Driver {
+  public static void main(String[] args) throws IOException {
+    Scanner sc = new Scanner(System.in);
+    int T = sc.nextInt();
+    while (T-- > 0) {
+      int n = sc.nextInt();
+     
+      Solution obj = new Solution();
+      long ans = obj.count(n);
+      System.out.println(ans);
+    }
+  }
 }
+
+public class Solution {
+  static long count(int n) {
+      return (long) Math.pow(2, (n*(n-1))/2);
+  }
+}
+
