330ee7b0495716a78feac865e777975559a0d40c
Prashant Balotra <69639884+balotraprashant@users.noreply.github.com>
2024-06-06 23:31:50 +0530

    Update Solution.java

--- src/arrays/medium/problem4/Solution.java
+++ src/arrays/medium/problem4/Solution.java
@@ -1,4 +1,65 @@
 package arrays.medium.problem4;
 
-public class Solution {
+class Solution {
+    public int maxSubArray(int[] nums) {
+        
+        // Brute Force Approach
+        // int maxSum = Integer.MIN_VALUE;
+        // for(int i=0; i<nums.length; i++) {
+        //     int sum = 0;
+        //     for(int j=i; j<nums.length; j++) {
+        //         sum += nums[j];
+        //         maxSum = Math.max(maxSum, sum);
+        //     }
+        // }
+
+        // return maxSum;
+
+        // Optimal Approach
+        // int maxSum = Integer.MIN_VALUE;
+        // int sum = 0;
+        // for(int i=0; i<nums.length; i++) {
+        //     sum += nums[i];
+
+        //     maxSum = Math.max(maxSum, sum);
+
+        //     if(sum<0) {
+        //         sum = 0;
+        //     }
+        // }
+
+        // return maxSum;
+
+        // Follow up Question
+        int maxSum = Integer.MIN_VALUE; // maximum sum
+        int sum = 0;
+
+        int start = 0;
+        int ansStart = -1, ansEnd = -1;
+        for (int i = 0; i < nums.length; i++) {
+
+            if (sum == 0) start = i; // starting index
+
+            sum += nums[i];
+
+            if (sum > maxSum) {
+                maxSum = sum;
+
+                ansStart = start;
+                ansEnd = i;
+            }
+
+            // If sum < 0: discard the sum calculated
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+
+        // To consider the sum of the empty subarray
+        // uncomment the following check:
+
+        //if (maxi < 0) maxi = 0;
+
+        return maxSum;
+    }
 }
