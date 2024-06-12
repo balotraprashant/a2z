b016e904a4acc0ad5b1f457703b453a9eb5ff12e
Prashant Balotra <69639884+balotraprashant@users.noreply.github.com>
2024-06-12 23:00:18 +0530

    Update Solution.java

--- src/arrays/medium/problem5/Solution.java
+++ src/arrays/medium/problem5/Solution.java
@@ -1,4 +1,26 @@
 package arrays.medium.problem5;
 
 public class Solution {
+  public int maxProfit(int[] prices) {
+
+        // Brute Force Approach
+        // int maxProfit = 0;
+        // for(int i=0; i<prices.length; i++) {
+        //     for(int j=i+1; j<prices.length; j++) {
+        //         int profit = prices[j] - prices[i];
+        //         maxProfit = Math.max(profit, maxProfit);
+        //     }
+        // }
+
+        // Optimal Approach
+        int maxProfit = 0;
+        int minProfit = Integer.MAX_VALUE;
+
+        for (int i=0; i<prices.length; i++) {
+            minProfit = Math.min(minProfit, prices[i]);
+            maxProfit = Math.max(maxProfit, prices[i]-minProfit);
+        }
+
+        return maxProfit;
+    }
 }
