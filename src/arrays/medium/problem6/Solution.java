23f533ae7fd887fdf4c4fc0f705bf5417a19df25
Prashant Balotra <69639884+balotraprashant@users.noreply.github.com>
2024-06-16 17:11:41 +0530

    Update Solution.java

--- src/arrays/medium/problem6/Solution.java
+++ src/arrays/medium/problem6/Solution.java
@@ -1,4 +1,35 @@
 package arrays.medium.problem6;
 
-public class Solution {
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        // Brute Force Approach
+        // ArrayList<Integer> positives = new ArrayList<>();
+        // ArrayList<Integer> negatives = new ArrayList<>();
+        // for(int i=0; i<nums.length; i++) {
+        //     if(nums[i]>=0) {
+        //         positives.add(nums[i]);
+        //     } else {
+        //         negatives.add(nums[i]);
+        //     }
+        // }
+
+        // for(int i=0; i<nums.length/2; i++) {
+        //     nums[2*i] = positives.get(i);
+        //     nums[2*i+1] = negatives.get(i);
+        // }
+        // return nums;
+
+        int[] ans = new int[nums.length];
+        int posIndex = 0, negIndex = 1;
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i]<0) {
+                ans[negIndex] = nums[i];
+                negIndex += 2;
+            } else {
+                ans[posIndex] = nums[i];
+                posIndex +=2;
+            }
+        }
+        return ans;
+    }
 }
