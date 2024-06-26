cbf139af703e3ff01f4d143b9714b4b27a37775d
Prashant Balotra <69639884+balotraprashant@users.noreply.github.com>
2024-06-26 23:42:32 +0530

    Update Solution.java

--- src/arrays/medium/problem9/Solution.java
+++ src/arrays/medium/problem9/Solution.java
@@ -1,4 +1,55 @@
 package arrays.medium.problem9;
 
-public class Solution {
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        
+        if(nums.length<1) return 0;
+
+        // My Approach
+
+        // TreeSet<Integer> ts = Arrays.stream(nums).boxed()
+        //                         .collect(Collectors.toCollection(TreeSet::new));
+
+        // Integer longest = 1;
+        // Integer longer = 1;
+
+        // Iterator<Integer> iterator = ts.iterator();
+        // if (iterator.hasNext()) {
+        //     Integer current = iterator.next();
+        //     while (iterator.hasNext()) {
+        //         Integer next = iterator.next();
+        //         if (next == current + 1) {
+        //             longer++;
+        //             longest = Math.max(longest, longer);
+        //         } else {
+        //             longer = 1;
+        //         }
+        //         current = next;
+        //     }
+        // }
+
+        // return (int)longest;
+
+        // Optimal Approach
+
+        int longest = 1;
+        HashSet<Integer> set = new HashSet<>();
+    
+        for(int element: nums) set.add(element);
+
+        for(int element: set) {
+            if(!set.contains(element-1)){
+                int count = 1;
+                int x = element;
+                while(set.contains(x+1)) {
+                    x++;
+                    count++;
+                }
+
+                longest = Math.max(count, longest);
+            }
+        }
+
+        return longest;
+    }
 }
