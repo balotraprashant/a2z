2d52acd5cb84a2be48f9aaad5a04b65ba7621075
Prashant Balotra <balotra.prashant@gmail.com>
2024-06-30 01:49:34 +0530

    Arrays medium 11

--- src/arrays/medium/problem11/Solution.java
+++ src/arrays/medium/problem11/Solution.java
@@ -1,4 +1,32 @@
 package arrays.medium.problem11;
 
-public class Solution {
+class Solution {
+    public void rotate(int[][] matrix) {
+        transpose(matrix);
+
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][m - 1 - j];
+                matrix[i][m - 1 - j] = temp;
+            }
+        }
+    }
+
+    public void transpose(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        for(int i=0; i<n; i++) {
+            for(int j=i; j<m; j++) {
+                if(i==j) continue;
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
 }
