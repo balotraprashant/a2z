beb0447de87cf1ed0b62e13ecb64480212dd375b
Prashant Balotra <69639884+balotraprashant@users.noreply.github.com>
2024-07-03 23:11:16 +0530

    Update Solution.java

--- src/arrays/medium/problem13/Solution.java
+++ src/arrays/medium/problem13/Solution.java
@@ -1,4 +1,28 @@
 package arrays.medium.problem13;
 
-public class Solution {
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> hm = new HashMap();
+        int prefixSum = 0;
+        int count = 0;
+
+        hm.put(0, 1);
+
+        for (int i = 0; i < nums.length; i++) {
+            // add current element to prefix Sum:
+            prefixSum += nums[i];
+
+            // Calculate x-k:
+            int remove = prefixSum - k;
+
+            // Add the number of subarrays to be removed:
+            count += hm.getOrDefault(remove, 0);
+
+            // Update the count of prefix sum
+            // in the map.
+            hm.put(prefixSum, hm.getOrDefault(prefixSum, 0) + 1);
+        }
+
+        return count;
+    }
 }
