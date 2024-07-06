4a885ed31d6a36bbdc84c87da1e4bfbe61629e30
Prashant Balotra <69639884+balotraprashant@users.noreply.github.com>
2024-07-06 19:21:01 +0530

    Update Solution.java

--- src/arrays/hard/problem2/Solution.java
+++ src/arrays/hard/problem2/Solution.java
@@ -1,4 +1,29 @@
 package arrays.hard.problem2;
 
-public class Solution {
+class Solution {
+
+    // My Bruteforce Approach
+    public List<Integer> majorityElement(int[] nums) {
+        ArrayList<Integer> res = new ArrayList<>();
+        int n = nums.length;
+        if(n<3) {
+            for(int i=0; i<n; i++) {
+                if(!res.contains(nums[i])) {
+                    res.add(nums[i]);
+                }
+            }
+            return res;
+        }
+
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for(int i=0; i<n; i++) {
+            hm.put(nums[i], hm.getOrDefault(nums[i], 0) + 1);
+        }
+
+        for(HashMap.Entry<Integer, Integer> entry: hm.entrySet()) {
+            if(entry.getValue() > n/3) res.add(entry.getKey());
+        }
+
+        return res;
+    }
 }
