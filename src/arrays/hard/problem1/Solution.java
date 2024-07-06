8f94154a7af5a37100dcd99f19fd5a87425f0030
Prashant Balotra <69639884+balotraprashant@users.noreply.github.com>
2024-07-06 18:50:40 +0530

    Update Solution.java

--- src/arrays/hard/problem1/Solution.java
+++ src/arrays/hard/problem1/Solution.java
@@ -1,4 +1,32 @@
 package arrays.hard.problem1;
 
-public class Solution {
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> row = new ArrayList<>();
+        row.add(1);
+        res.add(row);
+
+        for(int i=2; i<=numRows; i++) {
+            res.add(generateRow(i));
+        }
+
+        return res;
+    }
+
+    public List<Integer> generateRow(int rowNum) {
+        List<Integer> row = new ArrayList<>();
+        
+        int element = 1;
+        row.add(element);
+
+        for(int i=1; i<rowNum; i++) {
+            element = element * (rowNum-i);
+            element = element/i;
+
+            row.add(element);
+        }
+
+        return row;
+    }
 }
