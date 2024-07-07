11893d986c0a650fd812e28fcd444b2f85b22977
Prashant Balotra <69639884+balotraprashant@users.noreply.github.com>
2024-07-07 11:01:29 +0530

    Update Solution.java

--- src/arrays/hard/problem2/Solution.java
+++ src/arrays/hard/problem2/Solution.java
@@ -1,8 +1,6 @@
 package arrays.hard.problem2;
 
 class Solution {
-
-    // My Bruteforce Approach
     public List<Integer> majorityElement(int[] nums) {
         ArrayList<Integer> res = new ArrayList<>();
         int n = nums.length;
@@ -15,14 +13,53 @@ class Solution {
             return res;
         }
 
-        HashMap<Integer, Integer> hm = new HashMap<>();
+        // My Approach
+
+        // HashMap<Integer, Integer> hm = new HashMap<>();
+        // for(int i=0; i<n; i++) {
+        //     hm.put(nums[i], hm.getOrDefault(nums[i], 0) + 1);
+        // }
+
+        // for(HashMap.Entry<Integer, Integer> entry: hm.entrySet()) {
+        //     if(entry.getValue() > n/3) res.add(entry.getKey());
+        // }
+
+        // Optimal Approach - Extended Moore's Voting Alorithm
+
+        int el1 = Integer.MIN_VALUE;
+        int el2 = Integer.MIN_VALUE;
+
+        int count1 = 0;
+        int count2 = 0;
+        
         for(int i=0; i<n; i++) {
-            hm.put(nums[i], hm.getOrDefault(nums[i], 0) + 1);
+            if(count1==0 && nums[i]!=el2) {
+                count1 = 1;
+                el1 = nums[i];
+            } else if (count2==0 && nums[i]!=el1) {
+                count2 = 1;
+                el2 = nums[i];
+            } else if (nums[i]==el1) {
+                count1 ++;
+            } else if (nums[i]==el2) {
+                count2 ++;
+            } else {
+                count1--;
+                count2--; 
+            }
         }
 
-        for(HashMap.Entry<Integer, Integer> entry: hm.entrySet()) {
-            if(entry.getValue() > n/3) res.add(entry.getKey());
+        // Manually check if the stored elements in
+        // el1 and el2 are the majority elements:
+        count1 = 0; count2 = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == el1) count1++;
+            if (nums[i] == el2) count2++;
         }
+
+        int mini = (int)(n / 3) + 1;
+        if(count1 >= mini) res.add(el1);
+        if(count2 >= mini) res.add(el2);
 
         return res;
     }
