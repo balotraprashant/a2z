166a5fa1bafdc7faa8f272dd7513e7eaf54fb344
Prashant Balotra <69639884+balotraprashant@users.noreply.github.com>
2024-07-15 23:58:22 +0530

    Update Solution.java

--- src/stackandqueues/easy/problem2/Solution.java
+++ src/stackandqueues/easy/problem2/Solution.java
@@ -1,4 +1,73 @@
 package stackandqueues.easy.problem2;
 
-public class Solution {
+//{ Driver Code Starts
+import java.util.Scanner;
+
+public class Solution
+{
+	public static void main(String args[])
+	{
+		 Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		 while(t>0)
+		 {
+			MyQueue obj = new MyQueue();
+			int Q = sc.nextInt();
+			while(Q>0)
+			{
+				int QueryType = 0;
+				QueryType = sc.nextInt();
+				if(QueryType == 1)
+				{
+					int a = sc.nextInt();
+					
+					obj.push(a);
+					
+				}else if(QueryType == 2)
+				{
+				System.out.print(obj.pop()+" ");
+				}
+				Q--;
+			}
+			System.out.println("");
+			t--;
+		 }
+	}
 }
+
+
+
+
+// } Driver Code Ends
+
+
+
+
+class MyQueue {
+
+    int front, rear;
+	int arr[] = new int[100005];
+
+    MyQueue()
+	{
+		front=0;
+		rear=0;
+	}
+	
+	//Function to push an element x in a queue.
+	void push(int x)
+	{
+	    // Your code here
+	} 
+
+    //Function to pop an element from queue and return that element.
+	int pop()
+	{
+		// Your code here
+	} 
+}
+
+
+
+
+
