a87a2eecbe2894a6e217303c1acb8b1bd4bb5407
Prashant Balotra <69639884+balotraprashant@users.noreply.github.com>
2024-07-22 23:36:40 +0530

    Update Solution.java

--- src/stackandqueues/easy/problem8/Solution.java
+++ src/stackandqueues/easy/problem8/Solution.java
@@ -10,43 +10,87 @@ class Pair {
 
 class MinStack {
 
-    Stack<Pair> stack;
+    Stack <Long> st = new Stack<Long>();
+    Long mini;
 
+    /** initialize your data structure here. */
     public MinStack() {
-        stack = new Stack<>();
+        mini = Long.MAX_VALUE;
     }
-    
-    public void push(int val) {
-        int min;
-        if (stack.isEmpty()) {
-            min = val;
+
+    public void push(int value) {
+        Long val = Long.valueOf(value);
+        if (st.isEmpty()) {
+            mini = val;
+            st.push(val);
         } else {
-            min = Math.min(stack.peek().y, val);
+            if (val < mini) {
+                st.push(2 * val - mini);
+                mini = val;
+            } else {
+                st.push(val);
+            }
         }
-        stack.push(new Pair(val, min));
     }
-    
+
     public void pop() {
-        if(!stack.isEmpty()) {
-            stack.pop();
+        if (st.isEmpty()) return;
+
+        Long val = st.pop();
+        if (val < mini) {
+            mini = 2 * mini - val;
         }
     }
-    
+
     public int top() {
-        if(!stack.isEmpty()) {
-            return stack.peek().x;
+        Long val = st.peek();
+        if (val < mini) {
+            return mini.intValue();
         }
-        return -1;
+        return val.intValue();
     }
-    
+
     public int getMin() {
-        if(stack.isEmpty()) {
-            return -1;
-        } else {
-            Pair top = stack.peek();
-            return top.y;
-        } 
+        return mini.intValue();
     }
+
+    // Stack<Pair> stack;
+
+    // public MinStack() {
+    //     stack = new Stack<>();
+    // }
+    
+    // public void push(int val) {
+    //     int min;
+    //     if (stack.isEmpty()) {
+    //         min = val;
+    //     } else {
+    //         min = Math.min(stack.peek().y, val);
+    //     }
+    //     stack.push(new Pair(val, min));
+    // }
+    
+    // public void pop() {
+    //     if(!stack.isEmpty()) {
+    //         stack.pop();
+    //     }
+    // }
+    
+    // public int top() {
+    //     if(!stack.isEmpty()) {
+    //         return stack.peek().x;
+    //     }
+    //     return -1;
+    // }
+    
+    // public int getMin() {
+    //     if(stack.isEmpty()) {
+    //         return -1;
+    //     } else {
+    //         Pair top = stack.peek();
+    //         return top.y;
+    //     } 
+    // }
 }
 
 /**
