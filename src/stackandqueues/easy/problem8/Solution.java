9e258f7527ef146b2979543fa9a44c66d6675940
Prashant Balotra <69639884+balotraprashant@users.noreply.github.com>
2024-07-22 23:33:30 +0530

    Update Solution.java

--- src/stackandqueues/easy/problem8/Solution.java
+++ src/stackandqueues/easy/problem8/Solution.java
@@ -1,4 +1,59 @@
 package stackandqueues.easy.problem8;
 
-public class Solution {
+class Pair {
+    int x, y;
+    Pair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 }
+
+class MinStack {
+
+    Stack<Pair> stack;
+
+    public MinStack() {
+        stack = new Stack<>();
+    }
+    
+    public void push(int val) {
+        int min;
+        if (stack.isEmpty()) {
+            min = val;
+        } else {
+            min = Math.min(stack.peek().y, val);
+        }
+        stack.push(new Pair(val, min));
+    }
+    
+    public void pop() {
+        if(!stack.isEmpty()) {
+            stack.pop();
+        }
+    }
+    
+    public int top() {
+        if(!stack.isEmpty()) {
+            return stack.peek().x;
+        }
+        return -1;
+    }
+    
+    public int getMin() {
+        if(stack.isEmpty()) {
+            return -1;
+        } else {
+            Pair top = stack.peek();
+            return top.y;
+        } 
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
