9dd39ed34be9748b3c050626f871b7bce2cc45ce
Prashant Balotra <69639884+balotraprashant@users.noreply.github.com>
2024-07-25 22:56:31 +0530

    Update Solution.java

--- src/arrays/hard/problem5/Solution.java
+++ src/arrays/hard/problem5/Solution.java
@@ -1,4 +1,42 @@
 package arrays.hard.problem5;
 
-public class Solution {
+class Solution {
+    public int[][] merge(int[][] intervals) {
+
+        Arrays.sort(intervals, new Comparator<int[]>(){
+            public int compare(int[] a, int[] b) {
+                return a[0]-b[0];
+            }
+        });
+
+        ArrayList<int[]> res = new ArrayList<>();
+        int[] current = new int[2];
+
+        if(intervals.length>1){
+            current[0] = intervals[0][0];
+            current[1] = intervals[0][1];
+        } else {
+            return intervals;
+        }
+        
+
+        for(int i=1; i<intervals.length; i++) {
+            if(current[1]>=intervals[i][0] && current[1]<=intervals[i][1]){
+                current[1] = intervals[i][1];
+            } else {
+                if(current[1]<=intervals[i][1]) {
+                    res.add(current);
+                    current = new int[2];
+                    current[0] = intervals[i][0];
+                    current[1] = intervals[i][1];
+                }
+            }
+
+            if(i==intervals.length-1) {
+                res.add(current);
+            }
+        }
+
+        return res.toArray(new int[0][]);
+    }
 }
