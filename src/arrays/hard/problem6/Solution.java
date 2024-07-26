26e2e9c0818c1b63a83f61b4504b8e9147c99f5f
Prashant Balotra <69639884+balotraprashant@users.noreply.github.com>
2024-07-26 22:13:23 +0530

    Update Solution.java

--- src/arrays/hard/problem6/Solution.java
+++ src/arrays/hard/problem6/Solution.java
@@ -1,4 +1,17 @@
 package arrays.hard.problem6;
 
 public class Solution {
+  public void merge(int[] left, int m, int[] right, int n) {
+        int p1=m-1;
+        int p2=n-1;
+        int i=m+n-1;
+
+        while(p2>=0) {
+            if(p1>=0 && left[p1] > right[p2]){
+                left[i--] = left[p1--];
+            } else {
+                left[i--] = right[p2--];
+            }
+        }
+    }
 }
