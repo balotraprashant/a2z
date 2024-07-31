7ad45ed0723b314db2bd5470c0b2030802786a1a
Prashant Balotra <69639884+balotraprashant@users.noreply.github.com>
2024-07-31 23:09:49 +0530

    Update Solution.java

--- src/arrays/hard/problem7/Solution.java
+++ src/arrays/hard/problem7/Solution.java
@@ -33,24 +33,70 @@ class GFG {
 
 class Solve {
     int[] findTwoElement(int arr[], int n) {
-        // code here
-        int[] hash = new int[n + 1]; // hash array
+        // Brute Force
+        // int[] hash = new int[n + 1]; // hash array
 
-        //update the hash array:
+        // //update the hash array:
+        // for (int i = 0; i < n; i++) {
+        //     hash[arr[i]]++;
+        // }
+
+        // //Find the repeating and missing number:
+        // int repeating = -1, missing = -1;
+        // for (int i = 1; i <= n; i++) {
+        //     if (hash[i] == 2) repeating = i;
+        //     else if (hash[i] == 0) missing = i;
+
+        //     if (repeating != -1 && missing != -1)
+        //         break;
+        // }
+        // int[] ans = {repeating, missing};
+        // return ans;
+        
+        // Optimal Approach 1
+        int xr = 0;
+
+        //Step 1: Find XOR of all elements:
         for (int i = 0; i < n; i++) {
-            hash[arr[i]]++;
+            xr = xr ^ arr[i];
+            xr = xr ^ (i + 1);
         }
 
-        //Find the repeating and missing number:
-        int repeating = -1, missing = -1;
+        //Step 2: Find the differentiating bit number:
+        int number = (xr & ~(xr - 1));
+
+        //Step 3: Group the numbers:
+        int zero = 0;
+        int one = 0;
+        for (int i = 0; i < n; i++) {
+            //part of 1 group:
+            if ((arr[i] & number) != 0) {
+                one = one ^ arr[i];
+            }
+            //part of 0 group:
+            else {
+                zero = zero ^ arr[i];
+            }
+        }
+
         for (int i = 1; i <= n; i++) {
-            if (hash[i] == 2) repeating = i;
-            else if (hash[i] == 0) missing = i;
-
-            if (repeating != -1 && missing != -1)
-                break;
+            //part of 1 group:
+            if ((i & number) != 0) {
+                one = one ^ i;
+            }
+            //part of 0 group:
+            else {
+                zero = zero ^ i;
+            }
         }
-        int[] ans = {repeating, missing};
-        return ans;
+
+        // Last step: Identify the numbers:
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == zero) cnt++;
+        }
+
+        if (cnt == 2) return new int[] {zero, one};
+        return new int[] {one, zero};
     }
 }
