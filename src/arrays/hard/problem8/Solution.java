bf3ce9d21fa85deea36d86fd6ca3adceb723cd7c
Prashant Balotra <69639884+balotraprashant@users.noreply.github.com>
2024-08-04 21:36:03 +0530

    Update Solution.java

--- src/arrays/hard/problem8/Solution.java
+++ src/arrays/hard/problem8/Solution.java
@@ -1,1 +1,44 @@
 
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Sorting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long t = sc.nextLong();
+
+        while (t-- > 0) {
+            long n = sc.nextLong();
+            long arr[] = new long[(int)n];
+
+            for (long i = 0; i < n; i++) arr[(int)i] = sc.nextLong();
+
+            System.out.println(new Solution().inversionCount(arr, (int)n));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // arr[]: Input Array
+    // N : Size of the Array arr[]
+    // Function to count inversions in the array.
+    static long inversionCount(long arr[], int n) {
+        // Your Code Here
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] > arr[j]) cnt++;
+            }
+        }
+        return cnt;
+    }
+}
