346de15ad2c7b1f81a425ffdd02ae5b0fbb01157
Prashant Balotra <69639884+balotraprashant@users.noreply.github.com>
2024-08-05 23:18:38 +0530

    Update Solution.java

--- src/arrays/hard/problem9/Solution.java
+++ src/arrays/hard/problem9/Solution.java
@@ -1,4 +1,55 @@
 package arrays.hard.problem9;
 
-public class Solution {
+class Solution {
+    public int maxProduct(int[] nums) {
+        // int n = nums.length;
+        // int maxProduct = Integer.MIN_VALUE;
+        // int product = 1;
+        // for(int i=0; i<n; i++) {
+        //     product = product * nums[i];
+        //     if(product > maxProduct) {
+        //         maxProduct = product;
+        //     }
+        //     if(product<=0) {
+        //         product = 1;
+        //     }
+        // }
+
+        // return maxProduct;
+
+        // int max = nums[0], min = nums[0], ans = nums[0];
+        
+        // for (int i = 1; i < nums.length; i++) {
+            
+        //     int temp = max;  // store the max because before updating min your max will already be updated
+            
+        //     max = Math.max(Math.max(max * nums[i], min * nums[i]), nums[i]);
+        //     min = Math.min(Math.min(temp * nums[i], min * nums[i]), nums[i]);
+            
+        //     if (max > ans) {
+        //         ans = max;
+        //     }
+        // }
+        
+        // return ans;
+
+        int ans = nums[0];
+    int dpMin = nums[0];
+    int dpMax = nums[0];
+
+    for (int i = 1; i < nums.length; ++i) {
+      final int num = nums[i];
+      final int prevMin = dpMin;
+      final int prevMax = dpMax;
+      if (num < 0) {
+        dpMin = Math.min(prevMax * num, num);
+        dpMax = Math.max(prevMin * num, num);
+      } else {
+        dpMin = Math.min(prevMin * num, num);
+        dpMax = Math.max(prevMax * num, num);
+      }
+      ans = Math.max(ans, dpMax);
+    }
+    return ans;
+    }
 }
