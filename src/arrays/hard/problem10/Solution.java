cfff9d34039627f5d0a80bdbc0dea6b52df2403f
Prashant Balotra <69639884+balotraprashant@users.noreply.github.com>
2024-08-08 23:49:32 +0530

    Update Solution.java

--- src/arrays/hard/problem10/Solution.java
+++ src/arrays/hard/problem10/Solution.java
@@ -1,1 +1,17 @@
+class Solution {
+    public int reversePairs(int[] nums) {
 
+        // Brute Force
+        int n = nums.length;
+        int count = 0;
+        for(int i=0; i<n; i++) {
+            for(int j=i; j<n; j++) {
+                if(nums[i] > nums[j]*2) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
