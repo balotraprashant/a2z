3401b1ffe84be8ace1b6bf76d23c10c298478b23
Prashant Balotra <69639884+balotraprashant@users.noreply.github.com>
2024-08-09 23:47:20 +0530

    Update Solution.java

--- src/arrays/hard/problem10/Solution.java
+++ src/arrays/hard/problem10/Solution.java
@@ -1,17 +1,56 @@
 class Solution {
     public int reversePairs(int[] nums) {
+        int n = nums.length;
 
         // Brute Force
-        int n = nums.length;
-        int count = 0;
-        for(int i=0; i<n; i++) {
-            for(int j=i; j<n; j++) {
-                if(nums[i] > nums[j]*2) {
-                    count++;
-                }
-            }
-        }
+        
+        // int count = 0;
+        // for(int i=0; i<n; i++) {
+        //     for(int j=i; j<n; j++) {
+        //         if(nums[i] > nums[j]*2) {
+        //             count++;
+        //         }
+        //     }
+        // }
 
-        return count;
+        // return count;
+
+        return merger(0,n-1,nums);
+    }
+
+    private int merger(int start,int end,int ar[]){
+        if (start >= end) return 0;
+            int mid = (start + end) / 2;
+            int cnt = merger(start,mid,ar);
+            cnt += merger(mid+1,end,ar);
+            cnt += merge(start,mid,end,ar);
+            return cnt;
+        
+    }
+    
+    private int merge(int start,int mid,int end,int[] ar){
+        int max_cnt = 0;
+        int j = mid+1;
+        for (int i=start;i<=mid;i++){
+            while (j<=end && ar[i] > (2*(long)ar[j])) j++;
+            max_cnt += j - (mid+1);
+        }
+        
+        List<Integer> list = new ArrayList<>();
+        int i = start;
+        j = mid+1;
+        
+        while (i <= mid && j <= end){
+            if (ar[i] < ar[j]) list.add(ar[i++]);
+            else list.add(ar[j++]);
+        }
+        
+        while (i <= mid) list.add(ar[i++]);
+        while (j <= end) list.add(ar[j++]);
+        
+        for (int x=start;x<=end;x++){
+            ar[x] = list.get(x-start);
+        }
+        return max_cnt;
     }
 }
